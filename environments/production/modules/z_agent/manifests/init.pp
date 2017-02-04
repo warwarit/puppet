0af7bc0b7a6bfd8fbe9b6de2e1f9de3b5ddd9e73
warwarit <outroadx@gmail.com>
2017-02-04 17:28:26 +0200

    zavbix agent midle

---
+++ environments/production/modules/z_agent/manifests/init.pp
@@ -0,0 +1,25 @@
+class z_agent {
+	file {'/temp':
+		ensure	=> 'directory',
+	} 
+	file {"/temp/zabbix-release_3.0-1+jessie_all.deb":
+		require	=> file['/temp'],
+		ensure	=> file,
+		source	=> "puppet:///modules/z_agent/zabbix-release_3.0-1+jessie_all.deb",		
+	}
+	exec {'dpkg -i /temp/zabbix-release_3.0-1+jessie_all.deb':
+		require => file['/temp/zabbix-release_3.0-1+jessie_all.deb'],
+		command	=> 'dpkg -i /temp/zabbix-release_3.0-1+jessie_all.deb',
+		path	=> '/bin/:/sbin/:/usr/bin/:/usr/sbin/',
+	}
+	exec { 'apt-get update':
+		require => exec['dpkg -i /temp/zabbix-release_3.0-1+jessie_all.deb'],
+		command	=> 'apt-get update',
+		path    => '/bin/:/sbin/:/usr/bin/:/usr/sbin/',
+	}
+	exec { 'apt-get install -y zabbix-agent':
+		require	=> exec['apt-get update'],
+		command => 'apt-get install -y zabbix-agent',
+		path    => '/bin/:/sbin/:/usr/bin/:/usr/sbin/',
+	}	
+}
